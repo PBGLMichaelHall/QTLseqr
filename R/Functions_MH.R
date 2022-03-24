70697f2d0efe8e32fc69d69aff99e454e8f9e8b1
PBGLMichaelHall <michael.safe.browse@gmail.com>
2022-03-24 10:04:13 +0100

    commit

--- R/Functions_MH.R
+++ R/Functions_MH.R
@@ -899,6 +899,43 @@ plotQTLStats_MH <-
 
 
 
+#' @title Obs_Allele_Freq3
+#' @description Returns a 4 column data filtering for specific Chromosome and High Bulk Observed Allele Frequencies
+#' @param SNPSet A SNPSet generated from the function importFromTable 
+#' @export 
+
+
+Obs_Allele_Freq3 <- 
+  function (SNPSet) 
+  {
+    frame <- SNPSet %>% dplyr::mutate(LowRef = AD_REF.LOW, HighRef = AD_REF.HIGH, 
+                                      LowAlt = AD_ALT.LOW, HighAlt = AD_ALT.HIGH) %>% select(LowRef, 
+                                                                                             HighRef, LowAlt, HighAlt)
+    p1 <- ((frame$LowAlt)/(frame$LowRef + frame$LowAlt))
+    p1 <- round(p1, 3)
+    p2 <- ((frame$HighAlt)/(frame$HighRef + frame$HighAlt))
+    p2 <- round(p2, 3)
+    diff <- p2 - p1
+    Chrom <- SNPSet %>% select(CHROM)
+    POS <- SNPSet %>% select(POS)
+    AD_High1 <- data.frame(SNPSet$AD_ALT.HIGH, SNPSet$AD_REF.HIGH)
+    AD_High1$AD_High <- paste(AD_High1$SNPSet.AD_REF.HIGH, AD_High1$SNPSet.AD_ALT.HIGH, 
+                              sep = ",")
+    AD_High <- subset(AD_High1, select = -c(SNPSet.AD_ALT.HIGH, 
+                                            SNPSet.AD_REF.HIGH))
+    AD_Low1 <- data.frame(SNPSet$AD_REF.LOW, SNPSet$AD_ALT.LOW)
+    AD_Low1$AD_Low <- paste(AD_Low1$SNPSet.AD_REF.LOW, AD_Low1$SNPSet.AD_ALT.LOW, 
+                            sep = ",")
+    AD_Low <- subset(AD_Low1, select = -c(SNPSet.AD_REF.LOW, 
+                                          SNPSet.AD_ALT.LOW))
+    Gprime <- SNPSet %>% select(Gprime)
+    Gprime <- round(Gprime, 3)
+    data <- cbind(Chrom, POS, p1, p2, diff, AD_High, AD_Low, Gprime)
+    data <- as.data.frame(data)
+    return(data)
+  }
+
+
 
 
 
