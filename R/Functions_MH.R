f0fa6cf4b20725d852f2a52ff3c53a2bda396a3b
PBGLMichaelHall <michael.safe.browse@gmail.com>
2022-03-25 09:20:51 +0100

    commit

--- R/Functions_MH.R
+++ R/Functions_MH.R
@@ -442,29 +442,29 @@ Obs_Allele_Freq <- function(SNPSet){
 #' @export 
 
 Obs_Allele_Freq2 <- 
-  function (df_filt, ChromosomeValue, threshold) 
+  function (SNPSet, ChromosomeValue, threshold) 
   {
-    frame <- df_filt %>% dplyr::mutate(LowRef = AD_REF.LOW, HighRef = AD_REF.HIGH, 
-                                       LowAlt = AD_ALT.LOW, HighAlt = AD_ALT.HIGH) %>% select(LowRef, 
-                                                                                              HighRef, LowAlt, HighAlt)
+    frame <- SNPSet %>% dplyr::mutate(LowRef = AD_REF.LOW, HighRef = AD_REF.HIGH, 
+                                      LowAlt = AD_ALT.LOW, HighAlt = AD_ALT.HIGH) %>% select(LowRef, 
+                                                                                             HighRef, LowAlt, HighAlt)
     p1 <- ((frame$LowAlt)/(frame$LowRef + frame$LowAlt))
     p1 <- round(p1, 3)
     p2 <- ((frame$HighAlt)/(frame$HighRef + frame$HighAlt))
     p2 <- round(p2, 3)
     diff <- p2 - p1
-    Chrom <- df_filt %>% select(CHROM)
-    POS <- df_filt %>% select(POS)
-    AD_High1 <- data.frame(df_filt$AD_ALT.HIGH, df_filt$AD_REF.HIGH)
-    AD_High1$AD_High <- paste(AD_High1$df_filt.AD_REF.HIGH, AD_High1$df_filt.AD_ALT.HIGH, 
+    Chrom <- SNPSet %>% select(CHROM)
+    POS <- SNPSet %>% select(POS)
+    AD_High1 <- data.frame(SNPSet$AD_ALT.HIGH, SNPSet$AD_REF.HIGH)
+    AD_High1$AD_High <- paste(AD_High1$SNPSet.AD_REF.HIGH, AD_High1$SNPSet.AD_ALT.HIGH, 
                               sep = ",")
-    AD_High <- subset(AD_High1, select = -c(df_filt.AD_ALT.HIGH, 
-                                            df_filt.AD_REF.HIGH))
-    AD_Low1 <- data.frame(df_filt$AD_REF.LOW, df_filt$AD_ALT.LOW)
-    AD_Low1$AD_Low <- paste(AD_Low1$df_filt.AD_REF.LOW, AD_Low1$df_filt.AD_ALT.LOW, 
+    AD_High <- subset(AD_High1, select = -c(SNPSet.AD_ALT.HIGH, 
+                                            SNPSet.AD_REF.HIGH))
+    AD_Low1 <- data.frame(SNPSet$AD_REF.LOW, SNPSet$AD_ALT.LOW)
+    AD_Low1$AD_Low <- paste(AD_Low1$SNPSet.AD_REF.LOW, AD_Low1$SNPSet.AD_ALT.LOW, 
                             sep = ",")
-    AD_Low <- subset(AD_Low1, select = -c(df_filt.AD_REF.LOW, 
-                                          df_filt.AD_ALT.LOW))
-    Gprime <- df_filt %>% select(Gprime)
+    AD_Low <- subset(AD_Low1, select = -c(SNPSet.AD_REF.LOW, 
+                                          SNPSet.AD_ALT.LOW))
+    Gprime <- SNPSet %>% select(Gprime)
     Gprime <- round(Gprime, 3)
     data <- cbind(Chrom, POS, p1, p2, diff, AD_High, AD_Low, 
                   Gprime)
