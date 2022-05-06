889d6d1b42a57a58a2da38e8e207f670e89df40e
PBGLMichaelHall <michael.safe.browse@gmail.com>
2022-05-06 11:50:51 +0200

    commit

--- R/Functions_MH.R
+++ R/Functions_MH.R
@@ -939,5 +939,8 @@ Facet_Allelic_Chrom <- function(SNPset, subset = NULL, var1 = "AD_ALT.LOW",var2=
     p <- p + ggplot2::facet_grid(~CHROM, scales = "free_x")
   }
   p
+  data <- SNPset %>% dplyr::select(CHROM, POS, AD_ALT.LOW, AD_ALT.HIGH)
+  data <- as.data.frame(data)
+  return(data)
 }
 
